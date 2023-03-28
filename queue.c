20e0e211ba10bd604a6b4fe580a5522e3b3ba3e6
AlexF99 <alexandreopf@gmail.com>
2023-03-27 21:28:33 -0300

    implemented queue size

--- queue.c
+++ queue.c
@@ -1,6 +1,16 @@
 #include "queue.h"
 
-int queue_size (queue_t *queue) {}
+int queue_size (queue_t *queue) {
+    if (!queue) return 0;
+    int size = 1;
+    queue_t *head = queue;
+    queue_t *travel = queue->next;
+    while (travel != head) {
+        travel = travel->next;
+        size++;
+    }
+    return size;
+}
 
 void queue_print (char *name, queue_t *queue, void print_elem (void*) ) {}
 
