a8b88b8bb576d56c01f30e80a22076022f10cbb4
AlexF99 <alexandreopf@gmail.com>
2023-04-09 11:31:09 -0300

    implemented swapcontext

--- ppos_core.c
+++ ppos_core.c
@@ -2,7 +2,7 @@
 #include "ppos_data.h"
 #include <ucontext.h>
 
-task_t curr;
+task_t *curr;
 
 void ppos_init()
 {
@@ -11,11 +11,17 @@ void ppos_init()
 
 int task_init(task_t *task, void (*start_routine)(void *), void *arg) {}
 
-int task_switch(task_t *task) {}
+int task_switch(task_t *task)
+{
+    task_t *old = curr;
+    curr = task;
+    swapcontext(&(old->context), &(curr->context));
+    return 0;
+}
 
 void task_exit(int exit_code) {}
 
 int task_id()
 {
-    return curr.id;
+    return curr->id;
 }
