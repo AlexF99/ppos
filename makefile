8546b9784e46ce8d8072dd0167fe905bf36247da
AlexF99 <alexandreopf@gmail.com>
2023-04-09 12:09:35 -0300

    implemented task init, switch still with errors

--- makefile
+++ makefile
@@ -1,3 +1,4 @@
 run:
 	gcc -Wall -c queue.c -o queue.o
-	gcc -Wall queue.o testafila.c -o testafila
+	gcc -Wall -c ppos_core.c -o ppos_core.o
+	gcc -Wall queue.o ppos_core.o tests_ppos_tasks1.c -o teste
--- ppos_core.c
+++ ppos_core.c
@@ -1,15 +1,41 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "ppos_data.h"
 #include <ucontext.h>
 
-task_t *curr;
+#define STACKSIZE 64 * 1024
+
+task_t *curr, *MainTask;
 
 void ppos_init()
 {
     setvbuf(stdout, 0, _IONBF, 0);
 }
 
-int task_init(task_t *task, void (*start_routine)(void *), void *arg) {}
+int task_init(task_t *task, void (*start_routine)(void *), void *arg)
+{
+    char *stack = malloc(STACKSIZE);
+
+    getcontext(&(task->context));
+
+    if (stack)
+    {
+        (task->context).uc_stack.ss_sp = stack;
+        (task->context).uc_stack.ss_size = STACKSIZE;
+        (task->context).uc_stack.ss_flags = 0;
+        (task->context).uc_link = 0;
+    }
+    else
+    {
+        perror("Erro na criação da pilha: ");
+        exit(1);
+    }
+
+    makecontext(&(task->context), (void *)(*start_routine), 1, (char *)arg);
+    curr = task;
+
+    return 0;
+}
 
 int task_switch(task_t *task)
 {
@@ -19,7 +45,10 @@ int task_switch(task_t *task)
     return 0;
 }
 
-void task_exit(int exit_code) {}
+void task_exit(int exit_code)
+{
+    task_switch(MainTask);
+}
 
 int task_id()
 {
